604af344a59aee1eb8567203476e201dcb0b9d1a
MKT9 <kubit.magdalena@gmail.com>
2019-06-08 16:00:32 +0200

    Add OWM Api config

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -5,6 +5,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import net.aksingh.owmjapis.core.OWM;
+import net.aksingh.owmjapis.api.APIException;
+import net.aksingh.owmjapis.model.CurrentWeather;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -29,7 +32,6 @@ public class Main extends Application {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
         TestEntity model = new TestEntity();
-        model.setId(1);
         model.setDate("12.12.1990");
         model.setWeekDay("monday");
 
@@ -38,6 +40,26 @@ public class Main extends Application {
         entityManager.getTransaction().commit();
 
         System.out.println(entityManager.contains(model));
+
+        /* TEST POŁĄCZENIA Z API */
+        OWM owm = new OWM((OWMApiConfig.getInstance().getApiKey()));
+        owm.setUnit(OWM.Unit.METRIC);
+        System.out.println((OWMApiConfig.getInstance().getApiKey()));
+
+        try {
+            //szukamy miasta -> Radom
+            CurrentWeather cwd = owm.currentWeatherByCityName("Radom");
+
+            //Drukujemy nazwę miasta -> czy nam dobrze znalazło
+            System.out.println("Miasto: " + cwd.getCityName());
+
+            // printing the max./min. temperature
+            System.out.println("Temperatura: " + cwd.getMainData().getTempMax()
+                    + "/" + cwd.getMainData().getTempMin() + "\'C");
+        }
+        catch (APIException e) {
+            System.out.println(e.getMessage());
+        }
 
         launch(args);
 
