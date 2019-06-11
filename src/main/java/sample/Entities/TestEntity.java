52182fa11e99dfd0b77b076aae27bd6881e55641
MKT9 <kubit.magdalena@gmail.com>
2019-06-11 08:10:00 +0200

    Revert

---
+++ src/main/java/sample/Entities/TestEntity.java
@@ -0,0 +1,38 @@
+package sample.Entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class TestEntity {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String weekDay;
+    private String Date;
+
+    public String getWeekDay() {
+        return weekDay;
+    }
+
+    public void setWeekDay(String weekDay) {
+        this.weekDay = weekDay;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
