49c6b23c5c801ba582cc8c9e0d0edaa3ca7a5caf
kasia133 <nikielkasia@gmail.com>
2019-06-12 01:09:02 +0200

    a

--- src/main/java/sample/Controllers/Controller.java
+++ src/main/java/sample/Controllers/Controller.java
@@ -10,6 +10,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import sample.Config.CityDataInitializer;
 
+import static sample.Main.pogoda;
+
 public class Controller {
     @FXML
     private javafx.scene.control.ScrollPane s;
@@ -17,7 +19,24 @@ public class Controller {
     private javafx.scene.control.ComboBox miasto;
     @FXML
     private javafx.scene.text.TextFlow textMiasto;
-
+    @FXML
+    private javafx.scene.control.Label label1;
+    @FXML
+    private javafx.scene.control.Label label2;
+    @FXML
+    private javafx.scene.control.Label label3;
+    @FXML
+    private javafx.scene.control.Label label4;
+    @FXML
+    private javafx.scene.control.Label label5;
+    @FXML
+    private javafx.scene.control.Label label6;
+    @FXML
+    private javafx.scene.control.Label label7;
+    @FXML
+    private javafx.scene.control.Label label8;
+    @FXML
+    private javafx.scene.control.Label label9;
     @FXML
     private javafx.scene.control.Button mode;
 
@@ -31,7 +50,15 @@ public class Controller {
 
         Scene scene = new Scene(rootNode, 750, 560);
         scene.getStylesheets().add("/style/style2.css");
-
+        this.label1.setText(pogoda[0]);
+        this.label2.setText(pogoda[1]);
+        this.label3.setText(pogoda[2]);
+        this.label4.setText(pogoda[3]);
+        this.label5.setText(pogoda[4]);
+        this.label6.setText(pogoda[5]);
+        this.label7.setText(pogoda[6]);
+        this.label8.setText(pogoda[7]);
+        this.label9.setText(pogoda[8]);
         stage.setTitle("Weather App");
         stage.setScene(scene);
         stage.show();
